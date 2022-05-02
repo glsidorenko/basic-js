a31ed2adbe34c1f596e220ebbaf1603a5afbd795
gl.sidorenko <43067913+glsidorenko@users.noreply.github.com>
2022-05-03 01:57:11 +0300

    complete task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+
+  constructor(value) {
+
+    value === false ? this.type = 'reverse'
+    : this.type = 'direct';
+    this.mod = (n, m) => (n % m + m) % m
+    this.table= ["A","B","C","D","E",
+                "F","G","H","I","J","K","L","M",
+                "N","O","P","Q","R","S","T","U",
+                "V","W","X","Y","Z"];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  isInputValuesOk(message, key) {
+    if (!message || !key || typeof message !=='string' || typeof key !=='string') {
+      throw new Error('Incorrect arguments!');
+    } else {
+      return true;
+    }
+  }
+
+  encrypt(message, key) {
+    this.isInputValuesOk(message, key);
+    let charIndex =- 1,
+        encryptedMessage = message.toUpperCase().split('').map( elem => {
+      if (this.table.indexOf(elem) === -1) return elem;
+      charIndex++;
+      return this.table[this.mod((this.table.indexOf(elem)+this.table.indexOf(key.toUpperCase()[charIndex%key.length])),this.table.length)];
+    }).join('');
+    return this.type === 'direct' ? encryptedMessage 
+    : encryptedMessage.split('').reverse().join('');
+  }
+  
+  decrypt(encryptedMessage, key) {
+    this.isInputValuesOk(encryptedMessage, key);
+    let charIndex =-1,
+        message = encryptedMessage.toUpperCase().split('').map( elem => {
+      if (this.table.indexOf(elem)===-1) return elem;
+      charIndex++;
+      return this.table[Math.abs(this.mod((this.table.indexOf(elem) - this.table.indexOf(key.toUpperCase()[charIndex % key.length])),this.table.length))];
+    }).join('');
+    return this.type === 'direct' ? message 
+    : message.split('').reverse().join('');
   }
 }
 
